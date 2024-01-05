c02c3044b9e94adf1bca33632d5f5f022cdc5df7
ShashiS32 <46977833+ShashiS32@users.noreply.github.com>
2024-01-05 01:36:10 +0000

    fixed up game

--- main.py
+++ main.py
@@ -1,15 +1,99 @@
 import random
 
-
-word_list = ["ardvark", "baboon", "camel"]
+word_list = ['aarvark', 'baboon', 'camel']
 
 chosen_word = random.choice(word_list)
 
-guess = input("choose a letter: ").lower()
+wordlength = len(chosen_word)
 
-for letters in chosen_word:
-  if letters == guess:
-    print("Good Guess!")
-  else:
-    print("Bad Guess")
+lives = 6
+
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
+
+
+display = []
+
+for i in range (wordlength):
+  display += "_"
+
+endofgame = False
+
+while not endofgame:
+  guess = input("Guess a letter in the word: ").lower()
+
+
+
+  for position in range(wordlength):
+    letter = chosen_word[position]
+    if letter == guess:
+      display[position] = letter
+
+  if guess not in chosen_word:
+    lives -= 1
+    print(stages[lives])
+    if lives == 0:
+      endofgame = True
+      print("You lost")
+
+  print(display)
+
+
   
+  if "_" not in display:
+    endofgame = True
+    print("You win")
